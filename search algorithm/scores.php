bcf0033af12b20e531e1fb668eb3c7330e32e729
JynxSp0ck <jynxsp0ck@googlemail.com>
2013-08-06 15:23:37 +0100

    i made a mini search algorithm :3

---
+++ search algorithm/scores.php
@@ -0,0 +1,44 @@
+<?php
+	function calculateScores($activities, $conditions)
+	{
+		$weightings = Array(
+			new Weighting(0, 0),
+			new Weighting(1, 1),
+			new Weighting(-0.5, 0),
+			new Weighting(0.5, 1),
+			new Weighting(0, 1)
+		);
+		
+		$activitiesLength = sizeOf($activities);
+		
+		for ($i = 0; $i < activitiesLength; $i++)
+		{
+			$activities[$i]->score = 0;
+			$activities[$i]->score += $weightings[$activities[$i]->typeid]->condition * $conditions[$activities[$i]->typeid]->weather->condition;
+			$activities[$i]->score += $weightings[$activities[$i]->typeid]->temperature * $conditions[$activities[$i]->typeid]->weather->temperature;
+			$activities[$i]->score += $weightings[$activities[$i]->typeid]->precipitation * $conditions[$activities[$i]->typeid]->weather->precipitation;
+			
+			$activities[$i]->score += $weightings[$activities[$i]->typeid]->distance * $activities[$i]->distance;
+		}
+		
+		return $activities;
+	}
+	
+	class Weighting
+	{
+		public $condition;
+		public $temperature;
+		public $precipitation;
+		public $_time;
+		public $distance;
+		
+		function __construct($weather, $_time, $distance)
+		{
+			$this->condition = $weather / 3;
+			$this->temperature = $weather / 3;
+			$this->precipitation = -$weather / 3;
+			$this->_time = $_time;
+			$this->distance = $distance;
+		}
+	}
+?>
