b74554873d94b1a761a8b86ec2b08dc5d896ce35
Lawrie Cate <lawrie@catefamily.co.uk>
2013-08-07 11:48:58 +0100

    update to database keys

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '141.163.232.94';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'iambored';
