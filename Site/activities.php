f205bd53e1043090c1d43270069b6c0af587f38c
JynxSp0ck <jynxsp0ck@googlemail.com>
2013-08-09 18:19:34 +0100

    activities doesnt crash stuff when internets out

--- Site/activities.php
+++ Site/activities.php
@@ -1,6 +1,14 @@
 <?php
 	function getActivities($lat, $lon)
 	{
-		return json_decode(file_get_contents("http://things2do.ws/activities.json?lat=".$lat."&lon=".$lon));
+		$result = @file_get_contents("http://things2do.ws/activities.json?lat=".$lat."&lon=".$lon);
+		if ($result === FALSE)
+		{
+			return "-1";
+		}
+		else
+		{
+			return json_decode($result);
+		}
 	}
 ?>
