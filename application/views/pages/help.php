f381ca6c47cffccc8507132d301b83b0baa0c487
Lawrie Cate <lawrie@catefamily.co.uk>
2013-08-10 07:40:55 +0100

    help file

--- application/views/pages/help.php
+++ application/views/pages/help.php
@@ -1,1 +1,43 @@
-<h1>Things2Do Help</h1>
+<!doctype html>
+<head>
+<title>About things2do.ws</title>
+<style type="text/css">
+body,html {
+	font-family: sans-serif;
+}
+</style>
+</head>
+<body>
+	<h1>Things2Do Help &amp; Guide</h1>
+<h2>What Is It?</h2>
+<p>A search engine that looks for something that you would want to do - even if you don't know it!</p>
+<h2>How Do I Use It?</h2>
+<p>Just type in a simple request such as:</p>
+<ul>
+	<li>watch a film</li>
+	<li>go to the beach</li>
+	<li>shop</li>
+	<li>eat</li>
+</ul>
+<h2>How Does It Work?</h2>
+<p>The search algorithm takes in a selection of places in close proximity to your location.  Then, it evaluates each one based on how suited it is to a number of conditions including but not limited to:</p>
+<ul>
+	<li>The relevance to your query</li>
+	<li>The time</li>
+	<li>The weather</li>
+</ul>
+<p>The data is polled from a number of sources; currently the principle source of information is Foursquare which supplies most data about places.  However the system also interacts with a number of sources to help with determining results and supplementing them.  This includes:</p>
+<ul>
+	<li>Foursquare</li>
+	<li>moviesapi</li>
+	<li>Amazon</li>
+	<li>Met Datapoint</li>
+	<li>Geobytes</li>
+	<li>Alchemy Natural Language</li>
+</ul>
+<h2>What&apos;s Next?</h2>
+<p>We hope that things2do.ws can be improved by one day using your Facebook data to further personalise results or to suggest friends to accompany you.  Additional information about hyperlocal conditions such as traffic information is another way we might improve the system.</p>
+<h2>Who created &amp; nurtured this wondorous creation?</h2>
+<p>things2do.ws was made by Daniel Hart, Simon Barnes, Ryan Hockett, Micheal Maddocks &amp; Lawrie Cate</p>
+<p>We &hearts; <a href="http://youngrewiredstate.org/">YRS</a>!  Made for the 2013 Festival of Code</p>
+</body>
